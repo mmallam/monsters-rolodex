624be01e0f75d53673e514fd00b2a4c66dc22ec5
mmallam <123.meeting@gmail.com>
2020-07-16 09:42:57 -0400

    Updates

---
+++ precache-manifest.2a0cc4449660c98eda52363383b7671e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d666778cebbf043c5ebfc060b72fddfe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8eb4d650ee7a7d6e1a6b",
+    "url": "/monsters-rolodex/static/css/main.81eeb1e6.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb4d650ee7a7d6e1a6b",
+    "url": "/monsters-rolodex/static/js/main.57bae793.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
